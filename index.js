ba4dc0b7aa6e0c1d6587d622e3730ebb066081b9
Omar Alazaizeh <omx302@gmail.com>
2021-09-02 07:24:28 +0300

    bug fix

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const io = require("socket.io")(httpServer, {
     methods: ["GET", "POST"],
   },
 });
-let players = ["x", "o"];
+// let players = ["x", "o"];
 httpServer.listen(4000);
-
+let id;
 const uuid = require("uuid").v4; // random uuid
 
 let boardState = [
@@ -26,12 +26,14 @@ let gameState = "on";
 
 let objData = {};
 
+let players = [];
+
 io.on("connection", (socket) => {
   socket.on("reconnect", () => {
     objData.count = count;
     objData.boardState = boardState;
     objData.gameState = gameState;
-
+    objData.id = "o";
     console.log("*******************");
     console.log(objData);
     console.log("*******************");
@@ -43,40 +45,40 @@ io.on("connection", (socket) => {
     console.log(payload);
 
     //
+    console.log(players);
+
     console.log("connected getAll");
     let winerIs;
 
-    if (payload.count >= 7) {
-      for (let i = 0; i < boardState.length; i++) {
-        if ((boardState[i][0] == boardState[i][1]) == boardState[i][2]) {
-          gameState = "win";
-          winerIs = boardState[i][0];
-        } else if ((boardState[0][i] == boardState[1][i]) == boardState[2][i]) {
-          gameState = "win";
-          winerIs = boardState[0][i];
-        } else if (
-          (boardState[0][0] == boardState[1][1]) == boardState[2][2] ||
-          (boardState[0][2] == boardState[1][1]) == boardState[2][0]
-        ) {
-          gameState = "win";
-          winerIs = boardState[1][1];
-        }
-      }
-      if (gameState == "win") {
-        //
-      } else {
-        gameState = "tie";
-      }
-      boardState = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],
-      ];
-      gameState = "on";
-      count = 0;
-    } else {
-      payload.count = 0;
-    }
+    // if (payload.count >= 7) {
+    //   for (let i = 0; i < boardState.length; i++) {
+    //     if ((boardState[i][0] == boardState[i][1]) == boardState[i][2]) {
+    //       gameState = "win";
+    //       winerIs = boardState[i][0];
+    //     } else if ((boardState[0][i] == boardState[1][i]) == boardState[2][i]) {
+    //       gameState = "win";
+    //       winerIs = boardState[0][i];
+    //     } else if (
+    //       (boardState[0][0] == boardState[1][1]) == boardState[2][2] ||
+    //       (boardState[0][2] == boardState[1][1]) == boardState[2][0]
+    //     ) {
+    //       gameState = "win";
+    //       winerIs = boardState[1][1];
+    //     }
+    //   }
+    //   if (gameState == "win") {
+    //     //
+    //   } else {
+    //     gameState = "tie";
+    //   }
+    //   boardState = [
+    //     [0, 0, 0],
+    //     [0, 0, 0],
+    //     [0, 0, 0],
+    //   ];
+    //   gameState = "on";
+    //   count = 0;
+    // }
 
     count = objData.count = payload.count;
     boardState = objData.boardState = payload.boardState;
@@ -90,4 +92,8 @@ io.on("connection", (socket) => {
   });
 });
 
+io.on("disconnect", function () {
+  console.log("User disconnected:", io.id);
+  // socket.broadcast.emit("clients", io.engine.clientsCount);
+});
 // app.listen(4000, () => console.log("Live"));
